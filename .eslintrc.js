3d09eda83c75d1843a40ae83a0a24aa0042f84eb
Zaki <zakiabdulle1@gmail.com>
2023-02-18 13:58:30 +0000

    added linting for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	root: true,
+	"env": {
+		"browser": true,
+		"amd": true,
+		"node": true
+	},
+	"rules": {
+		"@typescript-eslint/explicit-function-return-type": "warn"
+	},
+	"ignorePatterns": ["/build/*"]
+  };
+  
